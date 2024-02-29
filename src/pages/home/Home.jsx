8c08db541afcd1d4ab8bf444110fa5e67f9133b8
admin <admin@admins-MacBook-Air.local>
2024-03-01 00:35:46 +0500

    new home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React, { memo } from "react";
+import "./Home.css";
+import book from "../../assets/book.jpeg";
 
 const Home = () => {
   return (
     <div className="home_page">
       <div className="container">
-        <div className="home_container">home</div>
+        <div className="home_container">
+          <div className="img_container">
+            <img src={book} alt="" />
+          </div>
+          <div className="buttons_container">
+            <button className="start">Start</button>
+            <button className="btn_next">Cilick</button>
+          </div>
+        </div>
       </div>
     </div>
   );
